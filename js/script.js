6bf0af1dadd854ed872acaed14b9f2a0edc6aede
Chris Flan <flanthedev@gmail.com>
2017-06-12 21:27:20 +1000

    initial commit

---
+++ js/script.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+
+    $("#content-div").load("home.html");
+
+    $("#home-nav").click(function(){
+        $("#content-div").load("home.html");
+        $("li").removeClass("active");
+        $("#home-nav").addClass("active");
+    });
+    $("#about-nav").click(function(){
+        $("#content-div").load("about.html");
+        $("li").removeClass("active");
+        $("#about-nav").addClass("active");
+
+    });
+    $("#portfolio-nav").click(function(){
+        $("#content-div").load("portfolio.html");
+        $("li").removeClass("active");
+        $("#portfolio-nav").addClass("active")
+
+        // Activate Carousel
+        $("#myCarousel").carousel();
+
+        // Enable Carousel Indicators
+        $(".item").click(function(){
+            $("#myCarousel").carousel(1);
+        });
+
+        // Enable Carousel Controls
+        $(".left").click(function(){
+            $("#myCarousel").carousel("prev");
+        });
+    });
+
+});
